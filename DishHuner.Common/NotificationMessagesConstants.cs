0fd54e7e6f3b0d1006ead9c0f34244b1747011ea
KristalinZH <kristalinzhelezchev@gmail.com>
2023-08-02 21:58:27 +0300

    Add toastr js library for notifications

---
+++ DishHuner.Common/NotificationMessagesConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace DishHunter.Common
+{
+	public class NotificationMessagesConstants
+	{
+		public const string ErrorMessage = "ErrorMessage";
+		public const string WarningMessage = "WarningMessage";
+		public const string InformationMessage = "InfoMessage";
+		public const string SuccessMessage = "SuccessMessage";
+	}
+}
