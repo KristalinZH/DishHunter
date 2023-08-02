ffd97d2e5605bab4bde6c2758593fffaa499ecb1
KristalinZH <kristalinzhelezchev@gmail.com>
2023-08-02 22:05:26 +0300

    Fix layout page and remove privacy action and page

---
+++ DishHuner.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace DishHunter.Common
+{
+	public static class GeneralApplicationConstants
+	{
+		public const int ReleaseYear = 2023;
+	}
+}
--- DishHunter.Web/Controllers/HomeController.cs
+++ DishHunter.Web/Controllers/HomeController.cs
@@ -19,11 +19,6 @@
 			return View();
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error(int statuscode)
 		{
